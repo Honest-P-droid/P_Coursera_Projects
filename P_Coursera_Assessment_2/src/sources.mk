9cb2172195f9e98a568566dd7c785db13ff2ea2b
Honest-P-droid <adithyaprasad52@gmail.com>
2026-06-29 22:46:56 +0530

    Update sources.mk
    Added helpful comments and minor bug fixes

--- P_Coursera_Assessment_2/src/sources.mk
+++ P_Coursera_Assessment_2/src/sources.mk
@@ -1,19 +1,11 @@
-#******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
-#
-# Redistribution, modification or use of this software in source or binary
-# forms is permitted as long as the files maintain this copyright. Users are 
-# permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
-#
-#****************************************************************************
-ifeq ($(PLATFORM) ,HOST)
+#Conditional statement for Host file selection
+ifeq ($(PLATFORM),HOST)
 	SOURCES = memory.c main.c
 	INCLUDES = -I ../include/common
 endif
 
-ifeq ($(PLATFORM) ,MSP432)
+#Conditional statement for MSP file selection
+ifeq ($(PLATFORM),MSP432)
 	SOURCES = main.c memory.c system_msp432p401r.c interrupts_msp432p401r_gcc.c startup_msp432p401r_gcc.c
 	INCLUDES = -I ../include/common -I ../include/msp432 -I ../include/CMSIS
 endif
